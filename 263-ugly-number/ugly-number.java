cbe4fce869bddeadfb10ec4173c39bf9f4dbd3da
Akashk-coder <akashkarikalan9080@gmail.com>
2025-07-19 09:57:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (76.23%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -4,17 +4,21 @@ class Solution {
         if(n<=0){
             return false;
         }
-        else if(n%2==0){
-            return isUgly(n/=2);
-        }
-        else if(n%3==0){
-            return isUgly(n/=3);
-        }
-        else if(n%5==0){
-            return isUgly(n/=5);
-        }
-        else{
-            return false;
-        }
+        // else if(n%2==0){
+        //     return isUgly(n/=2);
+        // }
+        // else if(n%3==0){
+        //     return isUgly(n/=3);
+        // }
+        // else if(n%5==0){
+        //     return isUgly(n/=5);
+        // }
+        // else{
+        //     return false;
+        // }
+        while(n%2==0)  n/=2;
+        while(n%3==0)  n/=3;
+        while(n%5==0)  n/=5;
+        return n==1;
     }
 }
