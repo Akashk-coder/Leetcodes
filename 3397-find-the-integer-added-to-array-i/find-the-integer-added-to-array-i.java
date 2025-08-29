c7ff7680e8d4b2759c4c2fb318d9ceaf93062306
Akashk-coder <akashkarikalan9080@gmail.com>
2025-08-29 14:20:13 +0530

    Time: 2 ms (59.61%) | Memory: 43.7 MB (7.75%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
@@ -10,6 +10,8 @@ class Solution {
         for(int j=0;j<nums2.length;j++){
             x2=Math.max(x2,nums2[j]);
         }
+        // int y=nums2[0]-nums1[0];
+        // return y;
         return x2-x1;
     }
 }
