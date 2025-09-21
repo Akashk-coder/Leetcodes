deecafe4f1c9637657f952adb997a74212517896
Akashk-coder <akashkarikalan9080@gmail.com>
2025-09-21 12:44:36 +0530

    Time: 7 ms (54.51%) | Memory: 41.9 MB (53.14%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character,Integer> map =new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        int length=0;
+        boolean odd = false;
+        for(int c : map.values()){
+            length+=(c/2)*2;
+        if(c%2==1){
+            odd=true;
+            }
+        }
+        if(odd) length++;
+        return length;
+    }
+}
