27878d49344c672fd58b551ec60ecdd4acf10b3d
Akashk-coder <akashkarikalan9080@gmail.com>
2025-09-21 12:45:22 +0530

    Time: 7 ms (54.51%) | Memory: 41.9 MB (53.14%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -2,7 +2,7 @@ class Solution {
     public int longestPalindrome(String s) {
         HashMap<Character,Integer> map =new HashMap<>();
         for(char c : s.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
+            map.put(c,map. getOrDefault(c,0)+1);
         }
         int length=0;
         boolean odd = false;
